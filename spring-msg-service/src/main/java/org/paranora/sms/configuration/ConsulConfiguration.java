faac1623d8ba9e381f5542babd413ae79ff69af7
paranora <yangqun@centaline.com.cn>
2021-05-20 14:03:21 +0800

    'u'

---
+++ spring-msg-service/src/main/java/org/paranora/sms/configuration/ConsulConfiguration.java
@@ -0,0 +1,16 @@
+package org.paranora.sms.configuration;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * @author :  paranora
+ * @description :  TODO
+ * @date :  2021/5/20 10:50
+ */
+@Configuration
+@EnableDiscoveryClient
+public class ConsulConfiguration {
+
+}
